1737345fede02318c8486fb678e170b267c1e1c7
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-10 17:54:57 +0100

    maybe

--- src/components/Info/sections/Software.vue
+++ src/components/Info/sections/Software.vue
@@ -56,6 +56,7 @@ export default defineComponent({
           this.$t('info.sections.software.langscategories.others'),
           'Java',
           'Python',
+          'Elixir',
           'Ruby',
         ],
       ] as string[][],
