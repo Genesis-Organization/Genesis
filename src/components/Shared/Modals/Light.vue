088815bc4fb767590d3629aaafd94f215442c6f4
Mateusz <mateuszslotwinski16@gmail.com>
2022-02-04 19:29:15 +0100

    Post modal

---
+++ src/components/Shared/Modals/Light.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="background" v-if="display">
+    <div class="bgclick" @click="toggleModal" />
+    <section class="modal">
+      <div class="content">
+        <slot />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      display: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.display = !this.display
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/index.scss';
+.background {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  z-index: 9;
+  background: #28353aa0;
+  backdrop-filter: blur(3.5px);
+}
+.modal {
+  width: 1000px;
+  height: 600px;
+  max-height: 90vh;
+  max-width: 95vw;
+  background: theme(dark);
+  border-radius: 20px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+.bar {
+  display: flex;
+  justify-content: space-between;
+  padding: 3px calc(15px + 3vw);
+  padding-bottom: 12px;
+  font-size: 24px;
+  color: theme(light);
+  .title {
+    font-weight: 500;
+  }
+  .close {
+    font-size: 27px;
+    cursor: pointer;
+  }
+}
+.content {
+  width: 100%;
+  flex-grow: 1;
+  background: theme(light);
+  border-radius: 20px;
+  padding: 10px;
+  text-align: left;
+}
+.bgclick {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
--- src/components/User/Profile/Blog/CreatePost.vue
+++ src/components/User/Profile/Blog/CreatePost.vue
@@ -10,10 +10,30 @@
           }"
         />
       </div>
-      <div class="write">
-        <div class="text" :placeholder="'hello'">Mój stary to fanatyk wędkarstwa, pół mieszkania zajebane wędkami. Najgorsze, średnio raz w miesiącuMój stary to fanatyk wędkarstwa, pół mieszkania zajebane wędkami. Najgorsze, średnio raz w miesiącu
-</div>
+      <div class="write" @click="showModal">
+        <div class="text" v-html="postContent ? postContent : 'Create Post'" />
       </div>
+      <Modal ref="modal">
+        <div id="modal_cont">
+          <div class="modal_bar">
+            <input v-model="postTitle" :placeholder="'Title'" />
+            <ic icon="times" @click="showModal" />
+          </div>
+          <textarea
+            v-model="postContent"
+            :placeholder="`I've recently discovered...`"
+          />
+          <div class="options_bar">
+            <div>
+              <button><ic icon="bold" /></button>
+              <button><ic icon="italic" /></button>
+              <button><ic icon="underline" /></button>
+              <button><ic icon="image" /></button>
+            </div>
+            <button v-html="$t('auth.inputs.confirm')" />
+          </div>
+        </div>
+      </Modal>
     </div>
     <div class="buttons"></div>
   </article>
@@ -21,12 +41,27 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Modal from '@/components/Shared/Modals/Light.vue'
 
 export default defineComponent({
+  components: {
+    Modal,
+  },
   data() {
     return {
       user: this.$store.getters.getUser,
+      postTitle: '',
+      postContent: '',
     }
+  },
+  methods: {
+    showModal() {
+      const modal = this.$refs.modal as InstanceType<typeof Modal>
+      if (modal) modal.toggleModal()
+    },
+  },
+  mounted() {
+    this.showModal()
   },
 })
 </script>
@@ -57,10 +92,6 @@ article {
   .text {
     width: 505px;
     font-size: 17px;
-    display: -webkit-box;
-    display: -moz-box;
-    display: -ms-box;
-    display: box;
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 2;
     overflow: hidden;
@@ -76,5 +107,55 @@ article {
   border-radius: 50%;
   background-size: cover;
   background-position: center;
+}
+
+#modal_cont {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  .modal_bar {
+    display: flex;
+    align-items: center;
+    font-size: 25px;
+    input {
+      padding: 3px 10px;
+      font-size: 21px;
+      border-bottom: 3px solid theme(gray);
+      flex-grow: 1;
+      margin-right: 10px;
+    }
+  }
+  textarea {
+    font-size: 19px;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    resize: none;
+  }
+  .options_bar {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 0;
+    div {
+      display: flex;
+      button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 32px;
+        height: 32px;
+        margin-right: 7px;
+      }
+    }
+    button {
+      padding: 6px 10px;
+      font-size: 17px;
+      background-color: theme(main_dark);
+      color: theme(light);
+      border-radius: 5px;
+    }
+  }
 }
 </style>
